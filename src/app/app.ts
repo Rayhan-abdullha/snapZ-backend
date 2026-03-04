3614b5db18ecebe250337d244d0faa313e121437
Rayhan-abdullha <captainrayhan215@gmail.com>
2026-03-05 02:30:22 +0600

    fix: issues

--- src/app/app.ts
+++ src/app/app.ts
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: ['http://localhost:5173', 'https://snap-z.vercel.app', 'http://localhost:3000'],
     optionsSuccessStatus: 200,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
